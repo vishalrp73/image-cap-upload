a496a8fee4699d8a650dc8091d6f7c862aecada8
Vishal Prakash <v.r.prakash73@gmail.com>
2022-02-23 11:02:28 +1300

    handles base64 image post in multipart / form-data

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {
   Text,
-  Button,
   Pressable,
   SafeAreaView,
   Image,
@@ -9,7 +8,7 @@ import {
   View,
   Dimensions
 } from 'react-native';
-import { launchCamera, launchImageLibrary, ImagePicker } from 'react-native-image-picker';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import axios from 'axios';
 
 const App = () => {
@@ -99,42 +98,44 @@ const App = () => {
         setImgList(images);
         console.log(imgList);
 
-        axios.post('https://damp-beach-36121.herokuapp.com/send', imgList)
+        // This POST will likely result in PayloadTooLargeError
+        /* axios.post('https://damp-beach-36121.herokuapp.com/send', imgList)
           .then (response => response.json())
           .then (json => console.log(json))
-          .catch (err => console.log(err));
+          .catch (err => console.log(err)); */
 
       }
     })
   }
 
-  const uploadImage = () => {
-    console.log('upload image button pressed')
-   
-    const image = {
-      uri: imageUri.uri,
-      type: 'image/jpeg',
-      name: 'custom_image_test.jpg'
-    }
-
-    const imgBody = new FormData();
-    imgBody.append('image', image);
-    const url = 'https://damp-beach-36121.herokuapp.com/send';
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'multipart/form-data',
-      },
-      body: imgBody
-    }).then (response => response.json())
-      .then (json => console.log(json))
-      .catch (err => console.log(err));
-
-
+  const selectImage = () => {
+    launchImageLibrary(options, response => {
+      if (response.didCancel) {
+        console.log('User cancelled operation')
+      } else if (response.error) {
+        console.log('ImagePickerError: ', response.error);
+      } else if (response.customButton) {
+        console.log('User pressed custom button: ', response.customButton)
+      } else {
+        const src = {uri: 'data:image/jpeg;base64,' + response.assets[0].base64};
+        setImageUri(src);
+        console.log(response.assets[0].base64)
+        multiPartPost(response.assets[0].base64);
+      }
+    })
   }
 
+  const multiPartPost = (base64) => {
+    const data = new FormData();
+    data.append("image_data", base64);
+
+    axios.post('https://damp-beach-36121.herokuapp.com/receiveBase64', data)
+    .then (response => console.log(response.data))
+    .catch (err => console.log(err));
+    
+  }
+
+  // Testing connection to server, and also provides URL template for axios functions
   const endpointTest = () => {
     axios.get('https://damp-beach-36121.herokuapp.com/')
     .then (response => console.log(response.data))
@@ -161,7 +162,7 @@ const App = () => {
                   pressed ? 'rgb(210, 230, 255)' : 'orange'},
                   styles.button
               ]}
-              onPress = {() => openLibrary() } >
+              onPress = {() => selectImage() } >
                 <Text style = {styles.text}>Open Library</Text>
             </Pressable>
             <Pressable
@@ -170,8 +171,8 @@ const App = () => {
                     pressed ? 'rgb(210, 230, 255)' : 'orange' },
                 styles.button
               ]}
-              onPress = {() => uploadImage() } >
-                <Text style = {styles.text}>Upload</Text>
+              onPress = {() => endpointTest() } >
+                <Text style = {styles.text}>Test Connection</Text>
             </Pressable>
           </View>
           <Image
